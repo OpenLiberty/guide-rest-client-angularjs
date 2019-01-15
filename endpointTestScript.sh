5866378c3a5b23e7647dc3c7cb0a1771737fc87d
Justine Chen <justine.chenibm.com@justines-mbp.war.can.ibm.com>
2019-01-15 11:57:47 -0500

    Testing correct endpoint

--- endpointTestScript.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-status="$(curl --write-out "%{http_code}\n" --silent --output /dev/null "http://localhost:9080/artists")";
-if [ "$status" == "200" ]; then
-   echo ENDPOINT OK;
-else
-   echo $status;
-   echo ENDPOINT NOT OK;
-   exit 1;
-fi
