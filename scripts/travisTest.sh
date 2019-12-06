7adb492f6e2c566842e0b4f07dfd1c24b0e98989
Eveline Cai <33134427+evelinec@users.noreply.github.com>
2019-12-06 16:44:38 -0500

    Minor clean up (#61)
    * Converted guide to dev mode
    
    * Update pom.xml
    
    * Update pom.xml
    
    * Minor clean up

--- scripts/travisTest.sh
+++ scripts/travisTest.sh
@@ -27,4 +27,3 @@ mvn liberty:start
 status="$(curl --write-out "%{http_code}\n" --silent --output /dev/null "http://localhost:9080/artists")"; if [ "$status" == "200" ]; then echo ENDPOINT OK; else echo "$status"; echo ENDPOINT NOT OK; exit 1; fi;
 
 mvn -q liberty:stop
-mvn failsafe:verify
